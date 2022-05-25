771c84ceeb9656e1b68a3bc70a0450e85874e55a
Igor <igorspirit2@gmail.com>
2022-05-25 15:15:26 +0300

    Add homework lecture 2_1

--- HomeWork/HW2_1.py
+++ HomeWork/HW2_1.py
@@ -1,1 +1,22 @@
-print("Hello world")
+a = 911
+    b = a % 10
+    print (b)
+    # b = 1
+c = str(b) + str(a)
+print (c)
+# c = 1911
+    x = int(c)//10
+    print (x)
+    # x = 191
+y = int(x) % 10
+print (y)
+# y = 1
+    z = str(y) + str(x)
+    print(z)
+    # z = 1191
+a1 = int(z)//10
+print (a1)
+# a1 = 119
+    print (a)
+    print (a1)
+
