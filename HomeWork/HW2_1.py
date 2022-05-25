d0c925e2b14ef4f87c8a9191cb5107b471e24d12
Igor <igorspirit2@gmail.com>
2022-05-25 23:20:53 +0300

    Add homework lecture 2_2

--- HomeWork/HW2_1.py
+++ HomeWork/HW2_1.py
@@ -1,22 +1,22 @@
 a = 911
-    b = a % 10
-    print (b)
+b = int(a) % 10
+print (b)
     # b = 1
 c = str(b) + str(a)
 print (c)
-# c = 1911
-    x = int(c)//10
-    print (x)
+    # c = 1911
+x = int(c)//10
+print (x)
     # x = 191
 y = int(x) % 10
 print (y)
-# y = 1
-    z = str(y) + str(x)
-    print(z)
+    # y = 1
+z = str(y) + str(x)
+print(z)
     # z = 1191
 a1 = int(z)//10
 print (a1)
-# a1 = 119
-    print (a)
-    print (a1)
+    # a1 = 119
+print (a)
+print (a1)
 
---
+++ HomeWork/HW2_2.py
@@ -0,0 +1,16 @@
+boys = 16
+girls = 18
+# boys - number of children in the 1st group
+# girls - number of children in the 2nd group
+# beds - ?
+
+boysbeds = 16//2
+print (boysbeds)
+# boysbeds = 8
+girlsbeds = 18//2
+print (girlsbeds)
+# girlsbeds = 9
+
+beds = boysbeds + girlsbeds
+print (beds)
+# beds = 17
