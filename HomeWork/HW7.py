9a80ccb0b0f5be13ae1b9f04a0165029fdc4aab4
Igor <igorspirit2@gmail.com>
2022-06-12 15:12:56 +0300

    Add HW9 to Homework

--- HomeWork/HW7.py
+++ HomeWork/HW7.py
@@ -1,3 +1,3 @@
 string = input("Enter your message: ")
 print(string and string[-1] == ".")
-print(len([i for i in string.split(". ")]))git
+print(len([i for i in string.split(". ")]))
---
+++ HomeWork/HW9.py
@@ -0,0 +1,33 @@
+import random
+
+n = int(input("Enter number:"))
+mylist = [random.randint(0,n) for i in range(n)]
+print(mylist)
+
+for count, value in enumerate(mylist):
+    print(count, value)
+
+n = int(input("Enter second number: "))
+index = mylist.index(int(n))
+
+print("index:", index)
+
+if n not in mylist:
+    print("Not found")
+elif mylist[0] == n:
+    print("number on the right:", mylist[index + 1])
+elif mylist[-1] == n:
+    print("number on the right:", mylist[index - 1])
+elif n in mylist:
+    print("numbers on the sides:", mylist[index - 1], "and", mylist[index + 1])
+else:
+    print("Not found")
+
+
+
+
+
+
+
+
+
