78de57f5b4eae1547a2e7df700c49b43a3408b79
Igor <igorspirit2@gmail.com>
2022-06-13 22:13:17 +0300

    Add correct(maybe) HW11 to HomeWork

--- HomeWork/HW11.py
+++ HomeWork/HW11.py
@@ -1,16 +1,10 @@
 names = [name for name in input("Enter names: ").split()]
 print(names)
+for count, value in enumerate(names):
+    print(count, value)
 
-def get_unique_names(names):
-    list_of_unique_names = []
-    unique_names = set(names)
-
-    for name in unique_names:
-        list_of_unique_names.append(name)
-
-    return list_of_unique_names
-
-print(get_unique_names(names))
+unique_names = [i for i in names if names.count(i) == 1]
+print(unique_names)
 
 
 
